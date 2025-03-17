02633d18d39aec0b385d0cfcd01ed8de598e13ea
eduardonunes92 <eduardonevangelista92@gmail.com>
2025-03-17 20:50:18 -0300

    Create ex-10.py

---
+++ ex-10.py
@@ -0,0 +1,6 @@
+#10 - problema: escreva um software informe a média aritimética de um aluno que cursou 4 bimestres
+nota1=int(input("qual a primeira nota?"))
+nota2=int(input("qual a segunda nota?"))
+nota3=int(input("qual a terceira nota?"))
+nota4=int(input("qual a quarta nota?"))
+print((nota1+nota2+nota3+nota4)/4)
