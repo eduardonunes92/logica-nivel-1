374f759b3db9d7a8ab75d9132aaea82816344746
eduardonunes92 <eduardonevangelista92@gmail.com>
2025-03-17 20:17:35 -0300

    Create ex-07.py

---
+++ ex-07.py
@@ -0,0 +1,10 @@
+# Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+
+print("       x")
+print("      xxx")
+print("     xxxxx")
+print("    xxxxxxx")
+print("   xxxxxxxxx")
+print("  xxxxxxxxxxx")
+print(" xxxxxxxxxxxxx ")
+print("xxxxxxxxxxxxxxx")
